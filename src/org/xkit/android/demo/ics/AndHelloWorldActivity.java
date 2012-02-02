fc29db8aa236c515d960f7f3024fe45970a8f70f
Hai <xscript@live.com>
2012-02-02 19:43:51 +0800

    add hello world project

---
+++ src/org/xkit/android/demo/ics/AndHelloWorldActivity.java
@@ -0,0 +1,12 @@
+package org.xkit.android.demo.ics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndHelloWorldActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
